cee2217c5ff0de117d792c60b87b1b70242ecf5a
n-vr <43145188+n-vr@users.noreply.github.com>
2024-01-18 23:24:50 +0100

    Expand tests for fast solution

--- digitsinrange_test.go
+++ digitsinrange_test.go
@@ -19,21 +19,23 @@ func BenchmarkCountDigitOccurancesInRange(b *testing.B) {
 	}
 }
 
-func TestCountDigitOccurancesInRangeNaive(t *testing.T) {
-	testCases := []struct {
-		d, n int
-		want int
-	}{
-		{5, 0, 0},
-		{5, 10, 1},
-		{5, 15, 1},
-		{5, 16, 2},
-		{5, 20, 2},
-		{5, 100, 20},
-		{5, 110, 21},
-		{5, 350, 65},
-	}
+var testCases = []struct {
+	d, n int
+	want int
+}{
+	{5, 0, 0},
+	{5, 0, 0},
+	{5, 10, 1},
+	{5, 15, 1},
+	{5, 16, 2},
+	{5, 20, 2},
+	{5, 100, 20},
+	{5, 110, 21},
+	{5, 350, 65},
+	{1, 13, 5},
+}
 
+func TestCountDigitOccurancesInRangeNaive(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(fmt.Sprintf("d%dn%d", tc.d, tc.n), func(t *testing.T) {
 			got := digitsinrange.CountDigitOccurancesInRangeNaive(tc.d, tc.n)
@@ -45,10 +47,18 @@ func TestCountDigitOccurancesInRangeNaive(t *testing.T) {
 }
 
 func TestCountDigitOccurancesInRange(t *testing.T) {
-	want := digitsinrange.CountDigitOccurancesInRangeNaive(9, 1250000)
-	got := digitsinrange.CountDigitOccurancesInRange(9, 1250000)
-
-	if got != want {
-		t.Errorf("CountDigitOccurancesInRange(9, 1250000) = %d; want %d", got, want)
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("d%dn%d", tc.d, tc.n), func(t *testing.T) {
+			match, naive, fast := matchApproaches(tc.d, tc.n)
+			if !match {
+				t.Errorf("CountDigitOccurancesInRange(%d, %d) = %d; want %d", tc.d, tc.n, fast, naive)
+			}
+		})
 	}
+}
+
+func matchApproaches(d, n int) (bool, int, int) {
+	naive := digitsinrange.CountDigitOccurancesInRangeNaive(d, n)
+	fast := digitsinrange.CountDigitOccurancesInRange(d, n)
+	return naive == fast, naive, fast
 }
